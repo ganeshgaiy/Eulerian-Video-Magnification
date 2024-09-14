e4425d3df68028a5b9857c78fb9c5971fe07e5b3
Ganesh Gaiy <ganesh944020@gmail.com>
2024-09-13 21:17:14 -0500

    to and fro functions, rgb and yiq

--- evm.py
+++ evm.py
@@ -2,35 +2,48 @@ import cv2 as cv #type: ignore
 import numpy as np #type: ignore
 
 image = cv.imread('./test-images/face-image.jpg')
-image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+# image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 image = cv.resize(image,(540,760))
 # cv.imshow('Image', image)
 # cv.waitKey(0)
 # cv.destroyAllWindows()
 
-# Task1:convert RGB Image to YIQ color space
-T_matrix = np.array([[0.299,0.59590059,0.21153661], [0.587,-0.27455667,-0.52273617], [0.114, -0.32134392, 0.31119955]])
-rows, cols = image.shape[:2]
+#YIQ transformation matrix
+rgb_to_yiq_matrix = np.array([
+    [0.299,  0.587,  0.114],
+    [0.596, -0.275, -0.321],
+    [0.212, -0.523,  0.311]
+])
 
-temp = np.zeros((rows, cols, 3), dtype=np.float64)
+yiq_to_rgb_matrix = np.array([
+        [1.0,  0.956,  0.621],
+        [1.0, -0.272, -0.647],
+        [1.0, -1.106,  1.703]
+    ])
 
-for i in range(rows):
-    for j in range(cols):
-        temp[i,j] = np.dot(T_matrix, image[i,j])
-
-cv.imshow('yiq',temp)
-cv.waitKey(0)
-# def rgb2ntsc(src):
-#     [rows,cols]=src.shape[:2]
-#     dst=np.zeros((rows,cols,3),dtype=np.float64)
-#     T = np.array([[0.114, 0.587, 0.298], [-0.321, -0.275, 0.596], [0.311, -0.528, 0.212]])
-#     for i in range(rows):
-#         for j in range(cols):
-#             dst[i, j]=np.dot(T,src[i,j])
-#     return dst
-
-# cv.imshow('yiq', rgb2ntsc(image))
-# cv.waitKey(0)
-
-
+# Todo: simplify code 1 & 1.1
 #1 convert the RGB video or image to YIQ color space
+def rgbtoyiq(rgb_image):
+    reshaped_img = rgb_image.reshape((-1, 3))
+    yiq_flat_image = np.dot(reshaped_img, rgb_to_yiq_matrix.T)
+    yiq_image = yiq_flat_image.reshape(rgb_image.shape)
+    return yiq_image
+
+#1.1 convert the YIQ video or image to RGB color space
+def yiqtorbg(yiq_image):
+    reshaped_img = yiq_image.reshape((-1,3))
+    rgb_flat_image = np.dot(reshaped_img, yiq_to_rgb_matrix.T)
+    rgb_img = rgb_flat_image.reshape(yiq_image.shape)
+    rgb_img = np.clip(rgb_img, 0, 255).astype(np.uint8)
+    return rgb_img
+
+yiq_img = rgbtoyiq(image)
+cv.imshow('image',yiq_img)
+cv.waitKey(0)
+
+cv.imshow('image2', yiqtorbg(yiq_img))
+cv.waitKey(0)
+
+
+
+
